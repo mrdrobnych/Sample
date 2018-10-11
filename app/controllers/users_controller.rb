493a03966ec0b4871f966af5254604f55dee851c
MD <mr.drobnych@gmail.com>
2018-10-11 13:10:48 +0300

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,50 @@
+class UsersController < ApplicationController
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			redirect_to root_path
+		end
+	end
+
+	def login
+		@user = User.new
+	end
+
+	def result
+		pp @user = User.find_by(email: params[:user][:email], password: params[:user][:password])
+		redirect_to user_path(@user.id)
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def edit
+		@user = User.find(params[:id])		
+	end
+
+	def update
+		@user = User.find(params[:id])
+		@user.update(user_params)
+		if @user.save
+			redirect_to user_path(@user.id)
+		end
+	end
+
+	def destroy
+		@user = User.find(params[:id])
+		@user.destroy
+		redirect_to root_path
+	end
+
+private
+	def user_params
+		params.require(:user).permit(:lastname, :firstname, :email, :password)
+	end
+
+end
